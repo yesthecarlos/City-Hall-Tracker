07c0237213a34412b934ee5443d964e5136efaca
yesthecarlos <carlosmendez86@gmail.com>
2021-07-27 08:52:59 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace City_Hall_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
