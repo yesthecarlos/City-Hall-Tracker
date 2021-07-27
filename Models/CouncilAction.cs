60d9528f6c0eb768d9c12a76231d3e3eda195b87
yesthecarlos <carlosmendez86@gmail.com>
2021-07-27 10:37:40 -0700

    add CouncilAction model

---
+++ Models/CouncilAction.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityHallTracker.Models
+{
+    public class CouncilAction
+    {
+      public CouncilAction()
+      {
+        this.JoinEntities = new HashSet<CouncilActionCouncilMember>();
+      }
+
+      public virtual ApplicationUser User { get; set; }
+
+      [Display(Name="Action Id")]
+      public int CouncilActionId { get; set; }
+
+      [Display(Name="Action Title")]
+      public string CouncilActionTitle { get; set; }
+
+      public string[] CouncilActionTags { get; set; }
+      public string[] CouncilActionYeas { get; set; }
+      public string[] CouncilActionNays { get; set; }
+
+      public virtual ICollection<CouncilActionCouncilMember> JoinEntities { get; set; }
+    }
+}
